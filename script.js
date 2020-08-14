c243d738642cd2be05a56b17251c5957477caf41
Yevhen Zalevskyy <360a3.eugenio@gmail.com>
2020-08-14 19:36:54 +0200

    timer in function

--- script.js
+++ script.js
@@ -25,68 +25,16 @@ Object.prototype.getTotalTaxes = function() {
 
 getTotalTaxes.call(litva)
 
-// getMySalary ===========================================
+// getMySalary ====== min = 1500, max = 2000 =============
 function getMySalary(country) {
   mySalary = {}
+  setInterval(() => {
   mySalary.salary = Math.round(1499.5 + Math.random() * 501) // min - 0.5 + Math.random() * (max - min + 1)
   mySalary.taxes = +(mySalary.salary * country.tax).toFixed(2)
   mySalary.profit = mySalary.salary - mySalary.taxes
-  return mySalary
+    console.log(mySalary)
+  }
+  , 10000)
 }
 
-// display every 10 seconds
-let timer = setInterval(() => console.log(getMySalary(litva)), 10000);
-// stop after 1 minute
-setTimeout(() => {clearInterval(timer)}, 61000);
-
-
-
-// ТУТ НЕ ДИВІТЬСЯ. ЦЕ ДЛЯ МЕНЕ))
-////////////////////////////////////////////////////////////////
-// const person = new Object({
-//   name: 'Ivan',
-//   age: 30,
-//   greet: function() {
-//     console.log('Greet!');
-//   },
-//   logInfo: function(job, phone) {
-//     console.group(`${this.name} info:`)
-//     console.log(`Name is ${this.name}`);
-//     console.log(`Age is ${this.age}`);
-//     console.log(`Job is ${job}`);
-//     console.log(`Phone is ${phone}`);
-//     console.groupEnd
-//   }
-// })
-//
-//
-// const lena = Object.create(person)
-// lena.name = 'Elena'
-// lena.age = 28
-//
-// const str = new String('I am string')
-//
-// person.logInfo()
-// person.logInfo.bind(lena)()
-// person.logInfo.bind(lena, 'Frontend', '34-612-21-4548')()
-// person.logInfo.call(lena, 'Frontend', '34-612-21-4548')
-// person.logInfo.apply(lena, ['Frontend', '34-612-21-4548'])
-//
-// /////======================
-//
-// const array = [1, 2, 3, 4, 5]
-//
-// // function multBy(arr, n) {
-// //   return arr.map(function(i) {
-// //     return i * n
-// //   })
-// // }
-// // console.log(multBy(array, 5));
-//
-// Array.prototype.multBy = function(n) {
-//   return this.map(function(i) {
-//     return i * n
-//   })
-// }
-// console.log(array.multBy(3));
-// console.log([1, 2, 3].multBy(2));
+getMySalary(litva);
